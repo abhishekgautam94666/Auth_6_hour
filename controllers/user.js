779827a791c5a22b54ecf10149d9e04bcde9e7bf
Abhishek <abhishekgautam94666@gmail.com>
2023-10-16 11:12:02 +0530

    user already exit

--- controllers/user.js
+++ controllers/user.js
@@ -4,17 +4,19 @@ import { sendCookie } from "../utils/features.js";
 import ErrorHandler from "../middlewares/error.js";
 
 // --------Register------------
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
     let user = await User.findOne({ email });
 
-    if (user) return next(new ErrorHandler("User already exits", 400));
+    if (user) {
+      return next(new ErrorHandler("User already exits", 400));
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user = await User.create({ name, email, password: hashedPassword });
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    user = await User.create({ name, email, password: hashedPassword });
-
-    sendCookie(user, res, "Register successfully", 201);
+      sendCookie(user, res, "Register successfully", 201);
+    }
   } catch (error) {
     res.status(404).json({
       success: false,
@@ -24,12 +26,11 @@ export const register = async (req, res) => {
 };
 
 // --------Login------------
-export const login = async (req, res) => {
+export const login = async (req, res,next) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email }).select("+password");
 
-    console.log(user);
     if (!user) {
       return next(new ErrorHandler("Invalid Email or password", 400));
     }
